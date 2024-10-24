2cf78b4c0463d7233fb9841e2a7c4c7648514db5
furukawa3152 <furukawa3152`gmail.com>
2024-10-24 19:00:48 +0900

    プロンプト修正。miniで行けそう。

--- nakashima.py
+++ nakashima.py
@@ -44,10 +44,11 @@ def read_textdata():
 def chat_gpt_return(input_data):
     client = OpenAI(api_key=chat_GPT_API_key)
     chat_completion = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=[{"role": "system",
-                   "content": "以下に与えるメールマガジンの文章を、内容ごとに分割して出力して下さい。分割した文章には日本語で内容を要約した簡潔な見出しをつけ、見出しをkeyに、内容をvalueに持つjson形式にしてください。valueの部分は絶対に要約せず、原文のままで出力すること。質問、回答形式になっている部分については、質問と回答をセットにして一つの内容として扱ってください。その際のkeyは質問内容の要約にしてください。質問内容、回答は要約せず原文のままで書き出すこと。回答は必ずjsonのみを出力すること。"},
-                  {"role": "user", "content": input_data}]
+                   "content": "以下に与えるメールマガジンの文章を、内容ごとに分割して出力して下さい。改行してあっても同一のトピックである場合もあるので、精査して同一の内容の場合は一つのvalueにすること。分割した文章には日本語で内容を要約した簡潔な見出しをつけ、見出しをkeyに、内容をvalueに持つjson形式にしてください。keyは必ず日本語です。valueの部分は絶対に要約せず、原文のままで出力すること。質問、回答形式になっている部分については、質問と回答をセットにして一つのvalueとして扱ってください。その際のkeyは質問内容の要約とし、頭に「質問コーナー：」とつけて下さい。質問内容、回答は要約せず原文のままで書き出すこと。回答のまえには改行を入れて下さい。jsonについては{'key1':'value1','key2':'value2'}という1層構造にし、これ以上ネストすることは固く禁止します。必ずjsonのみを出力すること。"},
+                  {"role": "user", "content": input_data}],
+        temperature=0.2,
     )
     return (chat_completion.choices[0].message.content,chat_completion.usage)
 
